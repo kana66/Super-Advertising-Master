7c32a1eab8214f7e9be819c8aa7ccb257af32fa4
kana <tailei.yang@bangtuike.com>
2018-08-10 19:07:29 +0800

    排名列表

--- src/pages/Home/components/Materials/components/RankingList/RankingList.jsx
+++ src/pages/Home/components/Materials/components/RankingList/RankingList.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import { Tab } from '@icedesign/base';
+import { Tab, Grid, Table, Pagination } from '@icedesign/base';
 
+const { Row, Col } = Grid;
 const TabPane = Tab.TabPane;
-
 const tabs = [
   { tab: "排名", key: "ranking", content: "这里是排名列表" },
   { tab: "我的", key: "my", content: "这里是我的列表" },
+];
+const dataSource = [
+  { ranking: "1", name: "王小毛", score: "1380", type: "团队", change: "up" },
+  { ranking: "2", name: "帮推客战队", score: "1200", type: "团队", change: "no_change" },
+  { ranking: "3", name: "星球公司", score: "1140", type: "个人", change: "down" },
+  { ranking: "4", name: "一", score: "900", type: "个人", change: "up" },
+  { ranking: "5", name: "二", score: "700", type: "团队", change: "down" },
+];
+const myDataSource = [
+  { ranking: "3", name: "王小毛", score: "1380", type: "团队", change: "up" },
+  { ranking: "56", name: "帮推客战队", score: "1200", type: "团队", change: "no_change" },
+  { ranking: "190", name: "星球公司", score: "1140", type: "个人", change: "down" },
 ];
 
 export default class RankingList extends Component {
@@ -28,14 +40,42 @@ export default class RankingList extends Component {
     console.log("click", key);
   }
 
+  handlePageChange(current) {
+    console.log(current)
+  }
+
+  nameRender(value, index, record) {
+    let column = record.ranking + '.' + record.name
+    return column;
+  }
+
   render() {
+    let _this = this;
     return (
       <Tab onChange={this.handleChange}>
-        {tabs.map(item => (
-          <TabPane key={item.key} tab={item.tab} onClick={this.handleClick}>
-            {item.content}
-          </TabPane>
-        ))}
+        {tabs.map(function(item) {
+          if (item.key == 'ranking') {
+            return <TabPane key={item.key} tab={item.tab}>
+              <Table dataSource={dataSource} hasBorder={false}>
+                <Table.Column title="名称" dataIndex="name" cell={_this.nameRender} />
+                <Table.Column title="分数" dataIndex="score" />
+                <Table.Column title="类型" dataIndex="type" />
+                <Table.Column title="变化" dataIndex="change" />
+              </Table>
+              <Pagination style={{ textAlign: 'center' }} shape="no-border" type="simple" onChange={_this.handlePageChange} />
+            </TabPane>
+          } else {
+            return <TabPane key={item.key} tab={item.tab}>
+              <Table dataSource={myDataSource} hasBorder={false}>
+                <Table.Column title="名称" dataIndex="name" cell={_this.nameRender} />
+                <Table.Column title="分数" dataIndex="score" />
+                <Table.Column title="类型" dataIndex="type" />
+                <Table.Column title="变化" dataIndex="change" />
+              </Table>
+              <Pagination style={{ textAlign: 'center' }} shape="no-border" type="simple" onChange={_this.handlePageChange} />
+            </TabPane>
+          }
+        })}
       </Tab>
     );
   }
