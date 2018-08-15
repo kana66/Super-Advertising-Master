cffac55ecba7dbc954bb1673b9586a57edd1de34
kana <tailei.yang@bangtuike.com>
2018-08-15 17:23:57 +0800

    调整账户详情页

--- src/pages/Home/components/Materials/components/Person/Person.jsx
+++ src/pages/Home/components/Materials/components/Person/Person.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tab, Grid, Feedback, Balloon, Button } from '@icedesign/base';
+import { Tab, Grid, Feedback, Balloon, Button, Dialog } from '@icedesign/base';
 import FoundationSymbol from 'foundation-symbol';
 import copy from 'copy-to-clipboard';
 
@@ -21,7 +21,8 @@ export default class Person extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      copyAddress: '0xb40EdcF24BDd7379a95739655f97564d36299616'
+      copyAddress: '0xb40EdcF24BDd7379a95739655f97564d36299616',
+      visibleIncreaseAmount: false,
     };
   }
   
@@ -39,8 +40,23 @@ export default class Person extends Component {
     Feedback.toast.success('复制成功。若失败，请手动复制。')
   }
 
+  onIncreaseAmountOpen = () => {
+    this.setState({
+      visibleIncreaseAmount: true
+    });
+  };
+
+  onIncreaseAmountClose = () => {
+    this.setState({
+      visibleIncreaseAmount: false
+    });
+  };
+
   render() {
     let _this = this
+    let footer = (
+      <a />
+    );
     return (
       <Tab onChange={this.handleChange}>
         {tabs.map(function(item) {
@@ -74,21 +90,47 @@ export default class Person extends Component {
               </Row>
               <Row wrap>
                 <Col align={"center"} style={{ textAlign: "center" }}>
-                  <Button size={"large"} type={"primary"} style={styles.addButton}>增加出租额度</Button>
+                  <Button 
+                    size={"large"} 
+                    type={"primary"} 
+                    style={styles.increaseButton} 
+                    onClick={_this.onIncreaseAmountOpen.bind(_this)}
+                  >
+                    增加出租额度
+                    <Dialog
+                      visible={_this.state.visibleIncreaseAmount}
+                      footer={footer}
+                      onClose={_this.onIncreaseAmountClose}
+                      style={styles.increaseAmount}
+                    >
+                      <Row>
+                        <Col style={{ fontSize: "16px", margin: "25px 25px 25px 10px" }}>
+                          充值地址：
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col style={{ fontSize: "14px", margin: "0px 0px 0px 15px", wordBreak: "break-all", wordWrap:"break-word" }}>
+                          {_this.state.copyAddress}&nbsp;&nbsp;&nbsp;&nbsp;
+                          <Balloon trigger={<FoundationSymbol type="copy" onClick={_this.copyAddress.bind(_this)}>复制</FoundationSymbol>} closable={false}>
+                            点击复制地址
+                          </Balloon>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col style={{ textAlign: "center", marginTop: "40px" }}>
+                          <img src={addressQrcode} width="128" height="128" alt="address" />
+                        </Col>
+                      </Row>
+                    </Dialog>
+                  </Button>
                 </Col>
               </Row>
             </TabPane>
           } else {
             return <TabPane key={item.key} tab={item.tab}>
               <Row wrap>
-                <Col>
-                  锁定1000PTT，预计收益1ETH
-                </Col>
-              </Row>
-              <Row wrap>
-                <Col>
-                  打入并锁定PTT1个月，产生可租借额度<br />
-                  {_this.state.copyAddress}
+                <Col style={styles.col}>
+                  充值地址 ： <span style={styles.span}>{_this.state.copyAddress}</span>
                   &nbsp;&nbsp;&nbsp;&nbsp;
                   <Balloon trigger={<FoundationSymbol type="copy" onClick={_this.copyAddress.bind(_this)} />} closable={false}>
                     点击复制地址
@@ -100,8 +142,13 @@ export default class Person extends Component {
                 </Col>
               </Row>
               <Row wrap>
-                <Col>
-                  800/1000 PTT-Silver
+                <Col style={styles.col}>
+                  已充值PTT ： <span style={styles.span}>1000</span>
+                </Col>
+              </Row>
+              <Row wrap>
+                <Col style={styles.col}>
+                  PTT-Silver ： <span style={styles.span}>100</span>/1000
                 </Col>
               </Row>
             </TabPane>
@@ -116,8 +163,10 @@ const styles = {
   col: {
     fontSize: "16px",
     margin: "25px 25px 25px 10px",
+    wordBreak: "break-all",
+    wordWrap:"break-word",
   },
-  addButton: {
+  increaseButton: {
     borderRadius: 0,
     margin: "26px",
     fontSize: "20px",
@@ -132,5 +181,10 @@ const styles = {
   creditDescSpan: {
     fontSize: "12px",
     color: "#A39F9E",
+  },
+  increaseAmount: {
+    height: 500,
+    width: 300,
+    padding: 10,
   },
 }
