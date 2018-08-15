9cd6dd5662b0322a97d94a89f2c0d7c305f9f000
kana <tailei.yang@bangtuike.com>
2018-08-15 17:23:18 +0800

    增加临时切换按钮

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,7 +10,9 @@ export default class Home extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isLogin: true,
+    };
   }
 
   // onWindowResize() {
@@ -26,13 +28,27 @@ export default class Home extends Component {
   //   window.removeEventListener('resize', this.onWindowResize)
   // }
 
+  onClick() {
+    if (this.state.isLogin) {
+      this.setState({ isLogin: false })
+    } else {
+      this.setState({ isLogin: true })
+    }
+  }
+
   render() {
     return (
       <div className="home-page" style={{ background: '#3E3A39' }}>
         <Header />
-        <LandingIntroBanner />
-        <Statistics />
-        <Materials />
+        {
+          this.state.isLogin ? <div>
+            <Materials />
+          </div> : <div>
+            <LandingIntroBanner />
+            <Statistics />
+          </div>
+        }
+        <div onClick={this.onClick.bind(this)}>切换</div>
         <Footer />
       </div>
     );
